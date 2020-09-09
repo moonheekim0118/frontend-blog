75c4b9b1f01af3ffb452de987f5350340efc8317
moonee <uuuu118@naver.com>
2020-09-09 17:21:15 +0900

    init

---
+++ content/__about/resume-en.md
@@ -0,0 +1,21 @@
+---
+title: 'about'
+date: '2020-09-09 12:10:00'
+lang: 'en'
+---
+
+# Moonhee Kim
+
+<p class="title">What I can do</p>
+<span class="skill">Vanilla JS</span> <span class="skill">HTML/CSS</span> <span class="skill">React</span> <span class="skill">Node JS</span> <span  class="skill">Express</span>
+<br></br>
+<br></br>
+<p class="title">contact</p>
+<div class="contact">
+    <p class="contactTitle">Github</p>
+    <a href="https://github.com/moonheekim0118">https://github.com/moonheekim0118</a>
+</div>
+<div class="contact">
+    <p  class="contactTitle">Email</p>
+    <p>moonhee118@gmail.com</p>
+</div>
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `moonee devlog🌛`,
+  description: `Blog posted about ...`,
+  author: `moonee`,
+  introduction: `프론트엔드 개발 공부 블로그`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `https://github.com/moonheekim0118`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'moonheekim0118/frontend-blog', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'moonee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
